1a07c1d5f32b2c33cfc32f03da8aae8f53615090
Henrique Souza <henrique.asouza@outlook.com>
2024-05-27 19:42:02 -0300

    test: add FindAuthenticatedUserController unit tests

---
+++ __tests__/unit/application/controllers/user/find-authenticated.spec.ts
@@ -0,0 +1,39 @@
+import { FindAuthenticatedUserController } from '@/application/controllers/user'
+import { MOCK_USER, UserStub } from '../../helpers'
+
+interface SutTypes {
+  sut: FindAuthenticatedUserController
+  userManager: UserStub
+}
+
+const makeSut = (): SutTypes => {
+  const userManager = new UserStub()
+  const sut = new FindAuthenticatedUserController(userManager)
+
+  return {
+    sut,
+    userManager
+  }
+}
+
+describe('Find Authenticated User Controller', () => {
+  it('should return statusCode 200 on success', async () => {
+    const { sut } = makeSut()
+    const res = await sut.handle({ userId: MOCK_USER.id })
+
+    expect(res).toMatchObject({
+      statusCode: 200,
+      data: MOCK_USER
+    })
+  })
+
+  it('should return statusCode 500 if findAuthenticatedUser throws', async () => {
+    const { sut, userManager } = makeSut()
+
+    jest.spyOn(userManager, 'getUser').mockRejectedValueOnce(new Error())
+
+    const res = await sut.handle({ userId: MOCK_USER.id })
+
+    expect(res.statusCode).toBe(500)
+  })
+})
