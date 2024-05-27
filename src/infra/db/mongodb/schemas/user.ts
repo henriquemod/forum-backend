a2ace4ff58da5b99a97df5e326bb2ff27043b127
Henrique Souza <henrique.asouza@outlook.com>
2024-05-27 14:00:40 -0300

    refactor: make user schema required params

--- src/infra/db/mongodb/schemas/user.ts
+++ src/infra/db/mongodb/schemas/user.ts
@@ -7,11 +7,12 @@ const userSchema = new mongoose.Schema<UserModel.Model>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   level: {
+    required: true,
     type: String,
     default: UserModel.Level.USER,
     enum: Object.values(UserModel.Level)
   },
-  verifiedEmail: { type: Boolean, default: false },
+  verifiedEmail: { type: Boolean, default: false, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 })
