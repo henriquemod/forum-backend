8761f0958365179b743b5818766cdf580b5b44a2
Henrique Souza <henrique.asouza@outlook.com>
2024-06-02 18:32:40 -0300

    test: add Find Reply Controller unit tests

--- __tests__/unit/application/helpers/reply-stub.ts
+++ __tests__/unit/application/helpers/reply-stub.ts
@@ -1,6 +1,14 @@
 import type { Reply } from '@/data/usecases/'
+import type { ReplyModel } from '@/domain/models'
+import { MOCK_REPLY } from '../../data/helpers'
 
-export class ReplyStub implements Reply.ReplyPost, Reply.Delete {
+export class ReplyStub
+  implements Reply.ReplyPost, Reply.Delete, Reply.FindById
+{
+  async findById(params: Reply.FindByIdParams): Promise<ReplyModel.Model> {
+    return MOCK_REPLY
+  }
+
   async delete(params: Reply.DeleteParams): Promise<void> {}
   async reply(_params: Reply.ReplyPostParams): Promise<void> {}
 }
