f7a7addd34dc61bd25316d78ac7bffa1e5a73c2f
Henrique Souza <henrique.asouza@outlook.com>
2024-06-02 18:37:29 -0300

    test: add Update Reply Controller unit tests

--- __tests__/unit/application/helpers/reply-stub.ts
+++ __tests__/unit/application/helpers/reply-stub.ts
@@ -3,12 +3,13 @@ import type { ReplyModel } from '@/domain/models'
 import { MOCK_REPLY } from '../../data/helpers'
 
 export class ReplyStub
-  implements Reply.ReplyPost, Reply.Delete, Reply.FindById
+  implements Reply.ReplyPost, Reply.Delete, Reply.FindById, Reply.Update
 {
   async findById(params: Reply.FindByIdParams): Promise<ReplyModel.Model> {
     return MOCK_REPLY
   }
 
+  async update(params: Reply.UpdateParams): Promise<void> {}
   async delete(params: Reply.DeleteParams): Promise<void> {}
   async reply(_params: Reply.ReplyPostParams): Promise<void> {}
 }
