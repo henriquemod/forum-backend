bd856d4ec7ba7ea14780a7d83209f49b8f67a6ee
Henrique Souza <henrique.asouza@outlook.com>
2024-06-02 18:27:16 -0300

    test: add Delete Reply Controller unit tests

--- __tests__/unit/application/helpers/reply-stub.ts
+++ __tests__/unit/application/helpers/reply-stub.ts
@@ -1,5 +1,6 @@
 import type { Reply } from '@/data/usecases/'
 
-export class ReplyStub implements Reply.ReplyPost {
+export class ReplyStub implements Reply.ReplyPost, Reply.Delete {
+  async delete(params: Reply.DeleteParams): Promise<void> {}
   async reply(_params: Reply.ReplyPostParams): Promise<void> {}
 }
